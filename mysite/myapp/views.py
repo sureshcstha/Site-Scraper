452a7efb52a6e233b48dfe17bd93cd3dd0748061
Suresh Shrestha <sureshcstha@gmail.com>
2020-11-09 11:41:52 -0700

    passing site for URL

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -10,7 +10,7 @@ def scrape(request):
     if request.method == 'POST':
         site = request.POST.get('site', '')
 
-        page = requests.get('https://facebook.com')
+        page = requests.get(site)
         soup = BeautifulSoup(page.text, 'html.parser')
 
         for link in soup.find_all('a'):
